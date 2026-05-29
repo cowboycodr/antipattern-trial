a55f3b20cd6d58b5ab6847dd3f91eeab0f6527ae
Kian McKenna <kian.p.m.007@gmail.com>
2026-05-29 08:42:24 -0500

    Add multiply helper

--- test/math.test.js
+++ test/math.test.js
@@ -1,5 +1,6 @@
 import assert from "node:assert/strict";
-import { add } from "../src/math.js";
+import { add, multiply } from "../src/math.js";
 
 assert.equal(add(1, 2), 3);
+assert.equal(multiply(3, 4), 12);
 console.log("math tests passed");
